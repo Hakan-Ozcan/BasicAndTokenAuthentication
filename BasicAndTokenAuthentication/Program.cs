ff8d9849ea42ebcdde64334d719382bb3ff07da2
RntService <opuqo@rentgo.com>
2023-09-20 15:52:30 +0300

    Add project files.

---
+++ BasicAndTokenAuthentication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "https://example.com/api/resource";
+        string username = "your_username";
+        string password = "your_password";
+
+        // Kullanıcı adı ve parola Base64 ile kodlanır
+        string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+
+        // HTTP isteği oluşturulur
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Headers.Add("Authorization", "Basic " + credentials);
+
+        try
+        {
+            // İstek gönderilir ve yanıt alınır
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine("Response: " + response.StatusDescription);
+        }
+        catch (WebException ex)
+        {
+            Console.WriteLine("Hata: " + ex.Message);
+        }
+    }
+}
+
---
+++ TokenAuthentication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string apiUrl = "https://example.com/api/resource";
+        string token = "your_access_token";
+
+        using (HttpClient client = new HttpClient())
+        {
+            // Belirteci isteğin "Authorization" başlığına eklenir
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            try
+            {
+                // GET isteği yapılır
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Response: " + content);
+                }
+                else
+                {
+                    Console.WriteLine("Hata: " + response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+        }
+    }
+}
+
